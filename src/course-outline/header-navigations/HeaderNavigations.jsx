d1a2362da09e4ea51f2f70c91770bc58ff65fddf
Cormilo <gitgitgithub@yandex.ru>
2025-02-06 00:28:19 +0300

    убрал лишнюю букву

--- src/course-outline/header-navigations/HeaderNavigations.jsx
+++ src/course-outline/header-navigations/HeaderNavigations.jsx
@@ -40,7 +40,7 @@ const HeaderNavigations = ({
             onClick={handleNewSection}
             disabled={errors?.outlineIndexApi}
           >
-            New moldule
+            New module
           </Button>
         </OverlayTrigger>
       )}
