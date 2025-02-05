ca541a022c826063f60aeb8cf7d569e76543e442
Cormilo <gitgitgithub@yandex.ru>
2025-02-05 13:10:39 +0300

    удалим help-sidebar-about

--- src/generic/help-sidebar/HelpSidebar.jsx
+++ src/generic/help-sidebar/HelpSidebar.jsx
@@ -39,68 +39,7 @@ const HelpSidebar = ({
   const groupConfigurationsDestination = generateLegacyURL(groupConfigurations);
 
   return (
-    <aside className={classNames('help-sidebar', className)}>
-      <div className="help-sidebar-about">{children}</div>
-      {showOtherSettings && (
-        <>
-          <hr />
-          <div className="help-sidebar-other">
-            <h4 className="help-sidebar-other-title">
-              {intl.formatMessage(messages.sidebarTitleOther)}
-            </h4>
-            <nav
-              className="help-sidebar-other-links"
-              aria-label={intl.formatMessage(messages.sidebarTitleOther)}
-            >
-              <ul className="p-0 mb-0">
-                {showOtherLink(scheduleAndDetails) && (
-                  <HelpSidebarLink
-                    pathToPage={scheduleAndDetailsDestination}
-                    title={intl.formatMessage(
-                      messages.sidebarLinkToScheduleAndDetails,
-                    )}
-                  />
-                )}
-                {showOtherLink(grading) && (
-                  <HelpSidebarLink
-                    pathToPage={gradingDestination}
-                    title={intl.formatMessage(messages.sidebarLinkToGrading)}
-                  />
-                )}
-                {showOtherLink(courseTeam) && (
-                  <HelpSidebarLink
-                    pathToPage={courseTeamDestination}
-                    title={intl.formatMessage(messages.sidebarLinkToCourseTeam)}
-                  />
-                )}
-                {showOtherLink(groupConfigurations) && (
-                  <HelpSidebarLink
-                    pathToPage={groupConfigurationsDestination}
-                    title={intl.formatMessage(
-                      messages.sidebarLinkToGroupConfigurations,
-                    )}
-                  />
-                )}
-                {showOtherLink(advancedSettings) && (
-                  <HelpSidebarLink
-                    pathToPage={advancedSettingsDestination}
-                    title={intl.formatMessage(messages.sidebarLinkToAdvancedSettings)}
-                  />
-                )}
-                {proctoredExamSettingsUrl && (
-                  <HelpSidebarLink
-                    pathToPage={proctoredExamSettingsUrl}
-                    title={intl.formatMessage(
-                      messages.sidebarLinkToProctoredExamSettings,
-                    )}
-                  />
-                )}
-              </ul>
-            </nav>
-          </div>
-        </>
-      )}
-    </aside>
+    ' '
   );
 };
 
