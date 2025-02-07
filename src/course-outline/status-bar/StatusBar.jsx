186c5563ae5ca2df4d43af0d8c2ef1674233e8ae
Cormilo <gitgitgithub@yandex.ru>
2025-02-07 17:32:48 +0300

    удалим d-flex flex-column justify-content-between

--- src/course-outline/status-bar/StatusBar.jsx
+++ src/course-outline/status-bar/StatusBar.jsx
@@ -17,12 +17,7 @@ import messages from './messages';
 import { getVideoSharingOptionText } from '../utils';
 
 const StatusBarItem = ({ title, children }) => (
-  <div className="d-flex flex-column justify-content-between">
-    <h5>{title}</h5>
-    <div className="d-flex align-items-center">
-      {children}
-    </div>
-  </div>
+  ' '
 );
 
 StatusBarItem.propTypes = {
